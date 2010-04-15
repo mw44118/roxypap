64d872ae93124d7c188cbd18f17e15fb858d8698
W. Matthew Wilson <matt@tplus1.com>
2010-04-14 20:21:55 -0400

    Changed how I set defaults

--- db/migrate/20100409184558_add_accepted_to_game.rb
+++ db/migrate/20100409184558_add_accepted_to_game.rb
@@ -1,6 +1,6 @@
 class AddAcceptedToGame < ActiveRecord::Migration
   def self.up
-    add_column :games, :accepted, :boolean, :default => 0
+    add_column :games, :accepted, :boolean, :default => false
   end
 
   def self.down
